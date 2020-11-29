d781cd22848a1891c3cc0129691c5e3aca22653f
rreisr00 <55236793+rreisr00@users.noreply.github.com>
2020-11-29 12:51:09 +0100

    Add files via upload

--- programa.c
+++ programa.c
@@ -9,32 +9,44 @@
 
 
 int calculaAleatorios(int min, int max);
+void reaccionHandler(int sig);
 void dosisHandler(int sig);
 
 
 int main(int argc, char* argv[]){
     pid_t pid, npid, pidwait, pidpaciente;
-    int estado;
+    int estado, npacientes;
     pid_t *pacientes;
     srand(time(NULL));
+
+    //Se crea el puntero a pacientes que guardara el pid de los pacientes creados por medico
     if(argc >= 2){
-        pacientes = (pid_t *)malloc(sizeof(pid_t)*atoi(argv[1]));
+        npacientes = atoi(argv[1]);
+        pacientes = (pid_t *)malloc(sizeof(pid_t)*npacientes);
     }
 
+    //Se crea un proceso hijo de
     pid = fork();
     if(pid == -1){
         perror("Llamada a fork()");
         exit(-1);
     }else if(pid == 0) /*MEDICO*/{
-        int pacientesCurados = 0;
+        if(signal(SIGUSR2, SIG_DFL) == SIG_ERR){
+            perror("Llamada a signal");
+            exit(-1);
+        }
+        signal(SIGKILL, SIG_DFL);
+        //Espera a que el proceso epidemiologo mande la señal
+        pause();
         //Crea a los procesos paciente
+        int darDosis, waitpaciente, reaccion, npacientesreaccion = 0;
         for(int i = 0; i < argv[1]; i++) {
             pidpaciente = fork();
             if(pidpaciente == -1){
                 perror("Llamada a fork()");
                 exit(-1);
             }else if(pidpaciente == 0) /*PACIENTE*/{
-                if(signal(SIGUSR1, dosisHandler) == SIG_ERR) {
+                if(signal(SIGUSR1, reaccionHandler) == SIG_ERR) {
                     perror("Llamada a signal");
                     exit(-1);
                 }
@@ -43,32 +55,42 @@ int main(int argc, char* argv[]){
                 pacientes[i] = pidpaciente;
             }
         }// Aqui solo sale medico
-        
-        if(signal(SIGUSR2, SIG_DFL) == SIG_ERR){
-            perror("Llamada a signal");
-            exit(-1);
-        }
-        pause();
-        for(int i = 0; i < argv[1]; i++){
 
+        for(int i = 0; i < npacientes; i++){
+            printf("Medico vacuna al paciente %d\n", i+1);
+            darDosis = kill(pacientes[i], SIGUSR1);
+            if(darDosis == -1){
+                perror("Llamada a kill()\n");
+                exit(-1);
+            }
+            waitpaciente = wait(&estado);
+            if(waitpaciente = -1){
+                perror("Llamada a wait()\n");
+                exit(-1);
+            }
+            reaccion = WEXITSTATUS(estado);
+            if(reaccion%2 = 0){
+                npacientesreaccion += 1;
+            }
         }
+        exit(npacientesreaccion);
     }else /*EPIDEMIOLOGO*/{
         npid = fork();
         if(npid == -1){
             perror("Llamada a fork()");
             exit(-1);
-        }else if(npidpid == 0)/*FARMACEUTICO*/{
+        }else if(npid == 0)/*FARMACEUTICO*/{
             if(signal(SIGUSR1, dosisHandler); == SIG_ERR) {
                 perror("La señal a fallado");
                 exit(-1);
             }
             pause();
         }else /*EPIDEMIOLOGO*/{
-            int dosis;
+            int dosis, killestado;
             sleep(2);
 
-            estado = kill(npid, SIGUSR1); //manda la señal al farmaceutico
-            if(kill == -1){
+            killestado = kill(npid, SIGUSR1); //manda la señal al farmaceutico
+            if(killestado == -1){
                 perror("Llamada a kill()");
                 exit(-1);
             }
@@ -76,17 +98,23 @@ int main(int argc, char* argv[]){
             dosis = WEXITSTATUS(estado);
             if(dosis == 0){
                 printf("No hay dosis suficiente para los pacientes");
-                kill(SIGKILL,npid);
-                kill(SIGKILL, pid);
+                kill(npid, SIGKILL);
+                kill(pid, SIGKILL);
                 exit(0);
             }else{
                 estado = kill(pid, SIGUSR2);
+                pidwait = wait(&estado);
+                printf("Se han curado %d paciente(s)\n", WEXITSTATUS(estado));
             }
         }
 
     }
 }
-
+void reaccionHandler(int sig){
+    sleep(2);
+    int reaccion = calculaAleatorios(1, 10);
+    exit(reaccion);
+}
 void dosisHandler(int sig){
     int dosis = calculaAleatorios(0, 1);
     exit(dosis);
